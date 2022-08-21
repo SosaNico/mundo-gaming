2d26e172944cb73b48faffb044fc13ba74d072bb
Nico Sosa <sosanicolasdaniel00@gmail.com>
2022-08-21 20:44:46 -0300

    Creando Landing

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TiShoppingCart } from 'react-icons/ti';
+
+const CartWidget = () => {
+return (
+    <><TiShoppingCart className='icon'/>4</>
+)
+}
+
+export default CartWidget;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+const ItemListContainer = ({title}) =>{
+    return(
+        <h1 className='text-center p-3'>{title}</h1>
+    )
+}
+
+export default ItemListContainer;
